f8a9539937e9d1bf875c79bb524bfc3b98b8350d
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-08-04 10:44:14 +0200

    Test of update of versions for codeship

--- yo/app/scripts/config.js
+++ yo/app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8080'})
+.constant('ENV', {name:'production',apiEndpoint:'http://pyrogress-env.elasticbeanstalk.com'})
 
 ;

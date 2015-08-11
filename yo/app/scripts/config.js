d4f2ef2ea164d262c23181f4fed9a8930212f4f8
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-08-11 16:48:05 +0200

    "Light" version of the landing page Replaced "friendly place" with "video review platform"

--- yo/app/scripts/config.js
+++ yo/app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://pyrogress-env.elasticbeanstalk.com'})
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8080'})
 
 ;

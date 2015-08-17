e0d33f40f87a1dbde1328d856e7912aa17de1d39
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-08-17 09:03:36 +0200

    Improved Analytics to: - Not take into account localhost data - Send an event when someone clicks on the newsletter registration to measure the conversion rate of the landing page

--- yo/app/scripts/controllers/home.js
+++ yo/app/scripts/controllers/home.js
@@ -6,5 +6,10 @@ angular.module('controllers').controller('HomePageCtrl', ['$scope', '$routeParam
 		if ($routeParams.thankyou) {
 			$scope.thankyou = true;
 		}
+
+		$scope.notifyGa = function() {
+			console.log("Notify ga");
+			ga('send', 'event', 'button', 'click', 'submit_newsletter', 1);
+		}
 	}
 ]);

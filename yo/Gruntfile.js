4d2028db826e0b90db2afd1117e82da804aa5cfe
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-09-01 18:12:12 +0200

    Small CSS fixes to work on grunt serve:dist

--- yo/Gruntfile.js
+++ yo/Gruntfile.js
@@ -175,7 +175,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/images/*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
@@ -440,7 +440,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('build', [
     'clean:dist',
-    'ngconstant:production',
+    'ngconstant:development',
     'wiredep',
     'useminPrepare',
     'concurrent:dist',

dc9589e6f90c279a57e9b989e9a717281f69b341
Thibaud Jobert <thibaud.jobert@gmail.com>
2015-10-05 10:57:02 +0200

    remove # in urls for better analytics and seo of pages

--- yo/app/scripts/app.js
+++ yo/app/scripts/app.js
@@ -25,9 +25,10 @@ var app = angular.module('app', [
   'RecursionHelper'
 ]);
 
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
 
-app.config(['$routeProvider',
-  function($routeProvider) {
     $routeProvider.
       when('/', {
         templateUrl: 'views/home-page.html',

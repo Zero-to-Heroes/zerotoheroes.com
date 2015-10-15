6487aad4f052bb025c4d715984dbb9b4503bf397
Thibaud Jobert <thibaud.jobert@gmail.com>
2015-10-15 17:41:04 +0200

    link to discussion room fixed intelligent tag sorting a>b>A:a>A:b>B:a

--- yo/app/scripts/controllers/upload.js
+++ yo/app/scripts/controllers/upload.js
@@ -142,7 +142,22 @@ angular.module('controllers').controller('UploadDetailsCtrl', ['$scope', '$route
 			var validTags = $scope.allowedTags.filter(function (el) {
 				return ~el.text.toLowerCase().indexOf($query);
 			});
-			return validTags;
+			return validTags.sort(function(a, b) {
+    			var tagA = a.text.toLowerCase();
+    			var tagB = b.text.toLowerCase();
+    			if (~tagA.indexOf(':')) {
+    				if (~tagB.indexOf(':')) {
+    					return (tagA < tagB) ? -1 : (tagA > tagB) ? 1 : 0;
+    				}
+    				return 1;
+    			}
+    			else {
+    				if (~tagB.indexOf(':')) {
+    					return -1;
+    				}
+    				return (tagA < tagB) ? -1 : (tagA > tagB) ? 1 : 0;
+    			}
+			});;
 		}
 
 		$scope.$watch('review.sport', function (newVal, oldVal) {

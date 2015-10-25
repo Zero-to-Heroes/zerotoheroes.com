c2d698ee9ed544dddaca8b85a5ec892dc1bf41b9
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-10-25 16:02:08 +0100

    Added notif in NewsRepository

--- src/main/java/com/coach/news/NewsRepository.java
+++ src/main/java/com/coach/news/NewsRepository.java
@@ -27,6 +27,9 @@ public class NewsRepository {
 		List<News> news = new ArrayList<>();
 
 		// Features
+		news.add(new News(new DateTime("2015-10-25").toDate(),
+				"You can now decide to be notified when a new review is added, or a comment added in a review",
+				News.Type.Feature));
 		news.add(new News(
 				new DateTime("2015-10-23").toDate(),
 				"It's now possible to independantly choose, for each video in a side-by-side comparison, which side you want to use (left, center, right)",

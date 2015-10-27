54104a702f98bd07c43c367a9283f8a090ab148a
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-10-27 10:51:09 +0100

    Fixed incorrect timestamp when adding sequence

--- yo/app/scripts/directives/video/sequenceController.js
+++ yo/app/scripts/directives/video/sequenceController.js
@@ -121,8 +121,8 @@ app.directive('sequenceController', ['$log', 'Api', '$modal', '$rootScope', 'ENV
 
 				$scope.addSequence = function() {
 					var params = {
-						sequenceStart1: $scope.sequenceStart1 ? $scope.sequenceStart1 : $scope.API.currentTime,
-						sequenceStart2: $scope.sequenceStart2 ? $scope.sequenceStart2 : $scope.API2.currentTime,
+						sequenceStart1: $scope.sequenceStart1 ? $scope.sequenceStart1 * 1000 : $scope.API.currentTime,
+						sequenceStart2: $scope.sequenceStart2 ? $scope.sequenceStart2 * 1000 : $scope.API2.currentTime,
 						video1position: $scope.params.video1position,
 						video2position: $scope.params.video2position,
 						speed: parseFloat($scope.params.speed),

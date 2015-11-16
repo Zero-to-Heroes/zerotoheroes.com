f591c2e89a9c8dc8ebd254ec47dc468deee6927a
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-11-16 20:04:43 +0100

    Fixed our pictures not being displayed on home page

--- yo/test/karma.conf.js
+++ yo/test/karma.conf.js
@@ -4,126 +4,129 @@
 // generator-karma 0.8.3
 
 module.exports = function(config) {
-  'use strict';
+	'use strict';
 
-  config.set({
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+	config.set({
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../',
+		// base path, that will be used to resolve files and exclude
+		basePath: '../',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+		// testing framework to use (jasmine/mocha/qunit/...)
+		frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/jquery/jquery.js',
-      'bower_components/angular/angular.js',
-      'bower_components/bootstrap/dist/js/bootstrap.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/videogular/videogular.js',
-      'bower_components/videogular-controls/vg-controls.js',
-      'bower_components/videogular-buffering/vg-buffering.js',
-      'bower_components/videogular-overlay-play/vg-overlay-play.js',
-      'bower_components/videogular-poster/vg-poster.js',
-      'bower_components/videogular-ima-ads/vg-ima-ads.js',
-      'bower_components/waypoints/waypoints.js',
-      'bower_components/SHA-1/sha1.js',
-      'bower_components/angulartics/src/angulartics.js',
-      'bower_components/angulartics/src/angulartics-adobe.js',
-      'bower_components/angulartics/src/angulartics-chartbeat.js',
-      'bower_components/angulartics/src/angulartics-cnzz.js',
-      'bower_components/angulartics/src/angulartics-flurry.js',
-      'bower_components/angulartics/src/angulartics-ga-cordova.js',
-      'bower_components/angulartics/src/angulartics-ga.js',
-      'bower_components/angulartics/src/angulartics-gtm.js',
-      'bower_components/angulartics/src/angulartics-kissmetrics.js',
-      'bower_components/angulartics/src/angulartics-mixpanel.js',
-      'bower_components/angulartics/src/angulartics-piwik.js',
-      'bower_components/angulartics/src/angulartics-scroll.js',
-      'bower_components/angulartics/src/angulartics-segmentio.js',
-      'bower_components/angulartics/src/angulartics-splunk.js',
-      'bower_components/angulartics/src/angulartics-woopra.js',
-      'bower_components/angulartics/src/angulartics-marketo.js',
-      'bower_components/angulartics/src/angulartics-intercom.js',
-      'bower_components/videogular-angulartics/vg-analytics.js',
-      'bower_components/angular-rangeslider/angular.rangeSlider.js',
-      'bower_components/angular-file-upload/dist/angular-file-upload.min.js',
-      'bower_components/angular-strap/dist/angular-strap.js',
-      'bower_components/angular-strap/dist/angular-strap.tpl.js',
-      'bower_components/angular-bootstrap-show-errors/src/showErrors.js',
-      'bower_components/moment/moment.js',
-      'bower_components/angular-scroll/angular-scroll.js',
-      'bower_components/angular-marked/angular-marked.js',
-      'bower_components/angular-logger/dist/angular-logger.min.js',
-      'bower_components/sprintf/dist/sprintf.min.js',
-      'bower_components/sprintf/dist/angular-sprintf.min.js',
-      'bower_components/aws-sdk-js/dist/aws-sdk.js',
-      'bower_components/angulartics-google-analytics/dist/angulartics-google-analytics.min.js',
-      'bower_components/angular-recursion/angular-recursion.js',
-      'bower_components/moment-duration-format/lib/moment-duration-format.js',
-      'bower_components/wowjs/dist/wow.js',
-      'bower_components/jquery.stellar/jquery.stellar.js',
-      'bower_components/angularjs-viewhead/angularjs-viewhead.js',
-      'bower_components/ng-tags-input/ng-tags-input.min.js',
-      'bower_components/angular-load/angular-load.js',
-      'bower_components/jquery-textcomplete/dist/jquery.textcomplete.js',
+		// list of files / patterns to load in the browser
+		files: [
+			'bower_components/jquery/dist/jquery.js',
+			'bower_components/jquery/jquery.js',
+			'bower_components/angular/angular.js',
+			'bower_components/bootstrap/dist/js/bootstrap.js',
+			'bower_components/angular-sanitize/angular-sanitize.js',
+			'bower_components/angular-touch/angular-touch.js',
+			'bower_components/angular-route/angular-route.js',
+			'bower_components/angular-resource/angular-resource.js',
+			'bower_components/angular-mocks/angular-mocks.js',
+			'bower_components/videogular/videogular.js',
+			'bower_components/videogular-controls/vg-controls.js',
+			'bower_components/videogular-buffering/vg-buffering.js',
+			'bower_components/videogular-overlay-play/vg-overlay-play.js',
+			'bower_components/videogular-poster/vg-poster.js',
+			'bower_components/videogular-ima-ads/vg-ima-ads.js',
+			'bower_components/waypoints/waypoints.js',
+			'bower_components/SHA-1/sha1.js',
+			'bower_components/angulartics/src/angulartics.js',
+			'bower_components/angulartics/src/angulartics-adobe.js',
+			'bower_components/angulartics/src/angulartics-chartbeat.js',
+			'bower_components/angulartics/src/angulartics-cnzz.js',
+			'bower_components/angulartics/src/angulartics-flurry.js',
+			'bower_components/angulartics/src/angulartics-ga-cordova.js',
+			'bower_components/angulartics/src/angulartics-ga.js',
+			'bower_components/angulartics/src/angulartics-gtm.js',
+			'bower_components/angulartics/src/angulartics-kissmetrics.js',
+			'bower_components/angulartics/src/angulartics-mixpanel.js',
+			'bower_components/angulartics/src/angulartics-piwik.js',
+			'bower_components/angulartics/src/angulartics-scroll.js',
+			'bower_components/angulartics/src/angulartics-segmentio.js',
+			'bower_components/angulartics/src/angulartics-splunk.js',
+			'bower_components/angulartics/src/angulartics-woopra.js',
+			'bower_components/angulartics/src/angulartics-marketo.js',
+			'bower_components/angulartics/src/angulartics-intercom.js',
+			'bower_components/videogular-angulartics/vg-analytics.js',
+			'bower_components/angular-rangeslider/angular.rangeSlider.js',
+			'bower_components/angular-file-upload/dist/angular-file-upload.min.js',
+			'bower_components/angular-strap/dist/angular-strap.js',
+			'bower_components/angular-strap/dist/angular-strap.tpl.js',
+			'bower_components/angular-bootstrap-show-errors/src/showErrors.js',
+			'bower_components/moment/moment.js',
+			'bower_components/angular-scroll/angular-scroll.js',
+			'bower_components/angular-marked/angular-marked.js',
+			'bower_components/angular-logger/dist/angular-logger.min.js',
+			'bower_components/sprintf/dist/sprintf.min.js',
+			'bower_components/sprintf/dist/angular-sprintf.min.js',
+			'bower_components/aws-sdk-js/dist/aws-sdk.js',
+			'bower_components/angulartics-google-analytics/dist/angulartics-google-analytics.min.js',
+			'bower_components/angular-recursion/angular-recursion.js',
+			'bower_components/moment-duration-format/lib/moment-duration-format.js',
+			'bower_components/wowjs/dist/wow.js',
+			'bower_components/jquery.stellar/jquery.stellar.js',
+			'bower_components/angularjs-viewhead/angularjs-viewhead.js',
+			'bower_components/ng-tags-input/ng-tags-input.min.js',
+			'bower_components/angular-load/angular-load.js',
+			'bower_components/jquery-textcomplete/dist/jquery.textcomplete.js',
+			'bower_components/angular-translate/angular-translate.js',
+			'bower_components/angular-translate-loader-url/angular-translate-loader-url.js'
+			'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
 
-      'app/scripts/app.js',
-      'app/scripts/landing/owl.carousel.min.js',
-      'app/scripts/landing/tweetie.min.js',
-      'app/scripts/landing/contact_me.js',
-      'app/scripts/landing/jqBootstrapValidation.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
+			'app/scripts/app.js',
+			'app/scripts/landing/owl.carousel.min.js',
+			'app/scripts/landing/tweetie.min.js',
+			'app/scripts/landing/contact_me.js',
+			'app/scripts/landing/jqBootstrapValidation.js',
+			'app/scripts/**/*.js',
+			'test/mock/**/*.js',
+			'test/spec/**/*.js'
+		],
 
-    // list of files / patterns to exclude
-    exclude: [],
+		// list of files / patterns to exclude
+		exclude: [],
 
-    // web server port
-    port: 9991,
+		// web server port
+		port: 9991,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [
-      'PhantomJS'
-    ],
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: [
+			'PhantomJS'
+		],
 
-    // Which plugins to enable
-    plugins: [
-      'karma-phantomjs-launcher',
-      'karma-jasmine'
-    ],
+		// Which plugins to enable
+		plugins: [
+			'karma-phantomjs-launcher',
+			'karma-jasmine'
+		],
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false,
 
-    colors: true,
+		colors: true,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// level of logging
+		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
-    // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
-  });
+		// Uncomment the following lines if you are using grunt's server to run the tests
+		// proxies: {
+		//   '/': 'http://localhost:9000/'
+		// },
+		// URL root prevent conflicts with the site root
+		// urlRoot: '_karma_'
+	});
 };

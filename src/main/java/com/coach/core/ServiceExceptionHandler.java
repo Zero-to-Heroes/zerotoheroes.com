6dfe6d872fe40bf68012f33b162e0763494c0c1e
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-11-19 11:48:58 +0100

    Remove exception handler notifications, it seems to be causing issues (but not in local tomcat though...)

--- src/main/java/com/coach/core/ServiceExceptionHandler.java
+++ src/main/java/com/coach/core/ServiceExceptionHandler.java
@@ -5,16 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.coach.core.notification.SlackNotifier;
 
-@ControllerAdvice
-@EnableWebMvc
+//@ControllerAdvice
+//@EnableWebMvc
 public class ServiceExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired

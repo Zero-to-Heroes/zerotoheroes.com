bd1996989af257de4086f1614fe429ca5cbe46f3
Sébastien Tromp <sebastien.tromp@gmail.com>
2015-11-19 19:23:27 +0100

    Going back to double scroll, as using a fixed position for the video prevents scrolling in case there is a lot of text :)

--- yo/app/scripts/app.js
+++ yo/app/scripts/app.js
@@ -272,7 +272,7 @@ app.run(['$rootScope', '$window', '$location', 'Localization', function($rootSco
 }]);
 
 
-/*app.directive('scrollable',  function ($window, $document, $log) {
+app.directive('scrollable',  function ($window, $document, $log) {
 	var $win = angular.element($window);
 	var windowHeight = $win.height();
 
@@ -329,7 +329,7 @@ app.run(['$rootScope', '$window', '$location', 'Localization', function($rootSco
 		  }
 		}
 	};
-});*/
+});
 
 
 var guid = function() {

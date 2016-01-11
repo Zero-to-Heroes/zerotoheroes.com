7018ab87a16fadc675dbef0d10d159a7806c2096
Sébastien Tromp <sebastien.tromp@gmail.com>
2016-01-11 17:08:02 +0100

    fix js error on home page for smooth scrolling (which isn't used by the way)

--- yo/app/scripts/landing/app2.js
+++ yo/app/scripts/landing/app2.js
@@ -1,22 +1,6 @@
 
 
 
-//smooth scroll
-$(function () {
-    $('.scroll-to a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-});
-
 //parallax
 $(window).stellar({
     horizontalScrolling: false,

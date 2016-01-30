e79063b33e6fe72ccdea68e6b4e43a7203a3d806
sebastientromp <sebastien.tromp@gmail.com>
2016-01-30 17:55:06 +0100

    hsreplay: add checkbox to try and show normally hidden cards - will not work if the card is never discovered during the game

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -42,7 +42,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse',
 						plugins: [
 							{name: 'parseCardsText', version: 2}, 
 							{name: 'parseDecks', version: 1}, 
-							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 7}
+							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 8}
 						],
 						customCss: 'hearthstone.css'
 					},

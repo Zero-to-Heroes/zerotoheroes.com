26fa66a4edde79ee57ec336599bb83d3a0ce05a1
sebastientromp <sebastien.tromp@gmail.com>
2016-02-04 10:55:09 +0100

    hsreplay: update version to force refresh

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -42,7 +42,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse',
 						plugins: [
 							{name: 'parseCardsText', version: 2}, 
 							{name: 'parseDecks', version: 1}, 
-							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 8}
+							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 9}
 						],
 						customCss: 'hearthstone.css'
 					},

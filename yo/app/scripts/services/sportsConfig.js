0f39717de744be58188c717c9b3ca50784a8fc99
sebastientromp <sebastien.tromp@gmail.com>
2016-02-06 11:23:35 +0100

    hsreplay: fix bug when going to some turns through comment shortcut causes an exception

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -42,7 +42,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse',
 						plugins: [
 							{name: 'parseCardsText', version: 2}, 
 							{name: 'parseDecks', version: 1}, 
-							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 10}
+							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 11}
 						],
 						customCss: 'hearthstone.css'
 					},

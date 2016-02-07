433c8526b39bee988b9ed0fd08da3e702126e7b4
sebastientromp <sebastien.tromp@gmail.com>
2016-02-07 21:24:06 +0100

    hsreplay: refactor action parsing into its own separate class hsreplay: show card draw

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -42,7 +42,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse',
 						plugins: [
 							{name: 'parseCardsText', version: 2}, 
 							{name: 'parseDecks', version: 1}, 
-							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 11}
+							{name: 'joustjs', player: true, format: ['text/plain', 'text/xml'], version: 12}
 						],
 						customCss: 'hearthstone.css'
 					},

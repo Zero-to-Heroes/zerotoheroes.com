97aa8907eb47441c3c708d089c7e223d777529fd
sebastientromp <sebastien.tromp@gmail.com>
2016-04-12 14:35:43 +0200

    fix o.criteria.search is undefined error

--- yo/app/scripts/controllers/videoListing.js
+++ yo/app/scripts/controllers/videoListing.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('controllers').controller('VideoListingCtrl', ['$scope', '$routeParams', 'Api', '$location', 'User', 'ENV', '$log', '$rootScope', '$route', 
-	function($scope, $routeParams, Api, $location, User, ENV, $log, $rootScope, $route) {
+angular.module('controllers').controller('VideoListingCtrl', ['$scope', '$routeParams', 'Api', '$location', 'User', 'ENV', '$log', '$rootScope', '$route', '$timeout', 
+	function($scope, $routeParams, Api, $location, User, ENV, $log, $rootScope, $route, $timeout) {
 		// $scope.videos = [];
 		$scope.ENV = ENV;
 		$scope.sport = $routeParams.sport;
@@ -23,30 +23,9 @@ angular.module('controllers').controller('VideoListingCtrl', ['$scope', '$routeP
 		}
 
 		$scope.search = function () {
-			// var hasCriteria = false
-
 			var params = $scope.criteria
-			$log.debug('params in parent', params)
-
 			params.sport = $scope.sport
-
 			params.ownVideos = $scope.ownVideos
-
-			//$log.log('triggering search', $location);
-			// if ($location.search().title) {
-			// 	// hasCriteria = true;
-			// 	params.title = $location.search().title;
-			// }
-
-			// if ($location.search().wantedTags) {
-			// 	// hasCriteria = true;
-			// 	params.wantedTags = $scope.unserializeTags($location.search().wantedTags);
-			// }
-
-			// if ($location.search().unwantedTags) {
-			// 	// hasCriteria = true;
-			// 	params.unwantedTags = $scope.unserializeTags($location.search().unwantedTags);
-			// }
 
 			Api.Sports.get({sport: $scope.sport}, function(data) {
 				$scope.subscribers = data.subscribers
@@ -54,104 +33,6 @@ angular.module('controllers').controller('VideoListingCtrl', ['$scope', '$routeP
 
 			$scope.criteria.search(params, true, null)
 		}
-
-		
-
-		// $scope.retrieveVideos = function(shouldGetOnlyMine, pageNumber, criteria) {
-		// 	var params = criteria ? criteria : {};
-		// 	$log.log('search with criteria', criteria)
-
-		// 	if ($scope.sport) 
-		// 		params.sport = $scope.sport;
-
-		// 	if (pageNumber) 
-		// 		params.pageNumber = pageNumber;
-			
-		// 	Api.ReviewsQuery.save(params, function(data) {
-		// 		$scope.videos = [];
-		// 		$scope.totalPages = data.totalPages;
-		// 		//$log.log('totalPages are ', $scope.totalPages);
-		// 		for (var i = 0; i < data.reviews.length; i++) {
-		// 			$scope.videos.push(data.reviews[i]);
-
-		// 			// $scope.countVideoComments(data.reviews[i]);
-		// 			// $scope.hasHelpfulComments(data.reviews[i]);
-
-		// 			if (data.reviews[i].tags) {
-		// 				data.reviews[i].tags.forEach(function(tag) {
-		// 					tag.sport = data.reviews[i].sport.key.toLowerCase();
-		// 				})
-		// 			}
-
-		// 		};
-		// 		$scope.range = $scope.getRange();
-
-		// 		// Update the URL
-		// 		$scope.updateUrl(params);
-		// 	});
-
-
-		// 	Api.Sports.get({sport: $scope.sport}, function(data) {
-		// 		$scope.subscribers = data.subscribers;
-		// 	});
-		// };
-
-		// $scope.updateUrl = function(params) {
-		// 	// cleariung params
-		// 	$location.search('');
-
-		// 	if (params.userName) $location.search('username', params.userName);
-		// 	if ($scope.criteria.wantedTags.length > 0) $location.search('wantedTags', $scope.serializeTags($scope.criteria.wantedTags));
-		// 	if ($scope.criteria.unwantedTags.length > 0) $location.search('unwantedTags', $scope.serializeTags($scope.criteria.unwantedTags));
-		// 	if ($scope.criteria.title) $location.search('title', $scope.criteria.title);
-		// }
-
-		// $scope.serializeTags = function(tags) {
-		// 	$log.log('serializing tags', tags);
-		// 	if (!tags) return '';
-
-		// 	var result = [];
-		// 	tags.forEach(function(tag) {
-		// 		result.push(tag.text);
-		// 	})
-		// 	return result;
-		// }
-
-		/*$rootScope.$on('user.logged.in', function() {
-			$scope.retrieveVideos('false', $scope.pageNumber);
-		});*/
-
-		// $scope.formatDate = function(date) {
-		// 	return moment(date).fromNow();
-		// }
-
-		// $scope.formatExactDate = function(date) {
-		// 	return moment(date).format("YYYY-MM-DD HH:mm:ss");;
-		// }
-
-		// $scope.upvoteReview = function(video) {
-		// 	Api.Reputation.save({reviewId: video.id, action: 'Upvote'},
-		// 		function(data) {
-		// 			video.reputation = data.reputation;
-		// 		}, 
-		// 		function(error) {
-		// 			// Error handling
-		// 			$log.error(error);
-		// 		}
-		// 	);
-		// }
-
-		// $scope.downvoteReview = function(video) {
-		// 	Api.Reputation.save({reviewId: video.id, action: 'Downvote'},
-		// 		function(data) {
-		// 			video.reputation = data.reputation;
-		// 		}, 
-		// 		function(error) {
-		// 			// Error handling
-		// 			$log.error(error);
-		// 		}
-		// 	);
-		// }
 
 		$scope.signUp = function() {
 			$rootScope.$broadcast('account.signup.show');
@@ -161,67 +42,11 @@ angular.module('controllers').controller('VideoListingCtrl', ['$scope', '$routeP
 			$rootScope.$broadcast('account.signin.show');
 		}
 
-		// $scope.buildUrl = function(video) {
-		// 	// Replace all special characters ex
-		// 	// http://stackoverflow.com/questions/9705194/replace-special-characters-in-a-string-with
-		// 	var url = '/r/' + video.sport.key.toLowerCase() + '/' + video.id + '/' + S(video.title).slugify().s;
-		// 	return url;
-		// }
-
 		$scope.$on('$routeChangeSuccess', function(next, current) {
 			if (current.$$route) {
 				$scope.ownVideos = current.$$route.ownVideos
 			}
 		})
-
-		// $scope.getRange = function() {
-		// 	var pages = [];
-			
-		// 	for (var i = -2; i <= 2; i++) {
-		// 		pages.push($scope.pageNumber + i);
-		// 	}
-
-		// 	//$log.log('first pages are', pages);
-		// 	// No negative pages
-		// 	if (pages[0] <= 0) {
-		// 		var offset = pages[0];
-		// 		for (var i = 0; i < pages.length; i++) {
-		// 			pages[i] = pages[i] - offset;
-		// 		}
-		// 	}
-		// 	else if (pages[pages.length - 1] > $scope.totalPages) {
-		// 		var offset = pages[pages.length - 1] - $scope.totalPages;
-		// 		for (var i = 0; i < pages.length; i++) {
-		// 			pages[i] = pages[i] - offset;
-		// 		}
-		// 	}
-
-		// 	//$log.log('pages are', pages);
-		// 	// Remove pages if there are too many of them
-		// 	while (pages[pages.length - 1] >= $scope.totalPages) {
-		// 		pages.splice(pages.length - 1, 1);
-		// 	}
-		// 	//$log.log('finally, apges are', pages);
-
-		// 	return pages;
-		// }
-
-		// $scope.goToPage = function(page) {
-		// 	//$log.log('going to page', page);
-		// 	//$log.log('routeparams', $routeParams);
-		// 	//$log.log('route is', $route);
-		// 	$route.updateParams({'pageNumber': page});
-		// 	//$scope.retrieveVideos($scope.tabs.activeTab, page);
-		// 	//$location.path('pageNumber', page);
-		// }
-
-		// $scope.goToPreviousPage = function() {
-		// 	$route.updateParams({'pageNumber': Math.max(1, $scope.pageNumber - 1)});
-		// }
-
-		// $scope.goToNextPage = function() {
-		// 	$route.updateParams({'pageNumber': Math.min($scope.totalPages, $scope.pageNumber + 1)});
-		// }
 
 		$scope.unsubscribe = function() {
 			Api.Subscriptions.delete({itemId: $scope.sport}, function(data) {
@@ -270,7 +95,17 @@ angular.module('controllers').controller('VideoListingCtrl', ['$scope', '$routeP
 					if (sequenceTag) {
 						$scope.criteria.unwantedTags.push(sequenceTag);
 					}
-					$scope.search();
+					$timeout(function() {
+						if (!$scope.criteria.search) {
+							$timeout(function() {
+								$scope.search()
+							}, 50)
+						}
+						else {
+							$scope.search()
+						}
+					})
+
 				}
 			);
 		}

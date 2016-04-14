a8400198bc16fb4f3b368a931f5c64bc66268ecf
sebastientromp <sebastien.tromp@gmail.com>
2016-04-14 17:16:55 +0200

    add some missing deck archetype tags

--- src/main/java/com/coach/tag/TagRepository.java
+++ src/main/java/com/coach/tag/TagRepository.java
@@ -104,7 +104,9 @@ public class TagRepository {
 		hearthstone.add(new Tag("Face Hunter"));
 		hearthstone.add(new Tag("Hunter Beast"));
 		hearthstone.add(new Tag("Camel Hunter"));
+		hearthstone.add(new Tag("Midrange Hunter"));
 		hearthstone.add(new Tag("Combo Druid"));
+		hearthstone.add(new Tag("Astral Druid"));
 		hearthstone.add(new Tag("Mill Druid"));
 		hearthstone.add(new Tag("Druid Ramp"));
 		hearthstone.add(new Tag("Egg Druid"));

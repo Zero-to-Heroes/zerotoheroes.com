c4c0da4565d4d78f2e9b76125372845e7aaf63c7
sebastientromp <sebastien.tromp@gmail.com>
2016-05-25 09:07:59 +0200

    fix bug the would cause the game state to update ahead of the relevant actions

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -44,7 +44,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse', 'localStorage
 						plugins: [
 							{name: 'parseCardsText', version: 8}, 
 							{name: 'parseDecks', version: 6}, 
-							{name: 'joustjs', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 43, dev: false},
+							{name: 'joustjs', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 45, dev: false},
 							{name: 'hsarenadraft', player: true, mediaType: 'arena-draft', version: 8}
 						],
 						customCss: 'hearthstone.css'

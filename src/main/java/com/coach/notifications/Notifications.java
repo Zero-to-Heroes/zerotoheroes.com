9ae6c3822281c003ac79124398268c6bc0428e1b
sebastientromp <sebastien.tromp@gmail.com>
2016-06-03 23:23:20 +0200

    automatically remove read notifications that are older than 28 days old

--- src/main/java/com/coach/notifications/Notifications.java
+++ src/main/java/com/coach/notifications/Notifications.java
@@ -1,7 +1,10 @@
 package com.coach.notifications;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import org.joda.time.DateTime;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,12 +13,24 @@ import lombok.Setter;
 @Getter
 public class Notifications {
 
+	private static final int MAX_DURATION_FOR_NOTIFICATIONS = 28;
+
 	private List<Notification> notifications = new ArrayList<>();
 	private int notificationId = 0;
 
 	public void addNotification(Notification notification) {
 		notification.setNotifId(notificationId++);
 		notifications.add(notification);
+
+		DateTime earliestDate = new DateTime().minusDays(MAX_DURATION_FOR_NOTIFICATIONS);
+
+		for (Iterator<Notification> iterator = notifications.iterator(); iterator.hasNext();) {
+			Notification notif = iterator.next();
+			if (new DateTime(notif.getCreationDate()).isBefore(earliestDate) && notif.getReadDate() != null) {
+				iterator.remove();
+			}
+
+		}
 	}
 
 	public List<Notification> filter(String type) {

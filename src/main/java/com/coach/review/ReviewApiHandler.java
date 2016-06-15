bd7e70d74adf494028a06e851e3faa968991d46e
sebastientromp <sebastien.tromp@gmail.com>
2016-06-15 15:04:32 +0200

    fixbug preventing anonymous use of the site

--- src/main/java/com/coach/review/ReviewApiHandler.java
+++ src/main/java/com/coach/review/ReviewApiHandler.java
@@ -215,8 +215,8 @@ public class ReviewApiHandler {
 		String currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
 		User user = userRepo.findByUsername(currentUser);
 		// Check that access is allowed
-		if (user == null || user.getId() == null || ("private".equalsIgnoreCase(review.getVisibility()) && review.getAuthorId() != null && !review.getAuthorId()
-				.equals(user.getId()))) { return new ResponseEntity<Review>(review, HttpStatus.FORBIDDEN); }
+		if ("private".equalsIgnoreCase(review.getVisibility()) && review.getAuthorId() != null && !review.getAuthorId()
+				.equals(user.getId())) { return new ResponseEntity<Review>(review, HttpStatus.FORBIDDEN); }
 
 		// Increase the view count
 		if (review.isTranscodingDone() || Sport.Meta.equals(review.getSport())) {

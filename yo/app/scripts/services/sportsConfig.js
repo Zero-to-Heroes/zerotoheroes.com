4801ed1a960244cff96f11a8744cd86d898f9f14
sebastientromp <sebastien.tromp@gmail.com>
2016-06-26 21:45:32 +0200

    fix bug when parsing mulligan in some extreme cases

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -62,7 +62,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse', 'localStorage
 						plugins: [
 							{name: 'parseCardsText', version: 12, dev: dev}, 
 							{name: 'parseDecks', version: 15, dev: dev}, 
-							{name: 'joustjs', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 57, dev: dev},
+							{name: 'joustjs', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 58, dev: dev},
 							{name: 'hsarenadraft', player: true, mediaType: 'arena-draft', version: 14, dev: dev}
 						],
 						customCss: 'hearthstone.css'

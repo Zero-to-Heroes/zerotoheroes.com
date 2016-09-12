5ebc291702adf14842a41f34149bef032cf9468e
sebastientromp <sebastien.tromp@gmail.com>
2016-09-12 13:25:31 +0200

    choose sorting options

--- yo/app/scripts/directives/review/commentDisplay.js
+++ yo/app/scripts/directives/review/commentDisplay.js
@@ -1,6 +1,6 @@
 var app = angular.module('app');
-app.directive('commentDisplay', ['$log', 'User', 'Api', '$parse', '$rootScope', '$timeout', 
-	function($log, User, Api, $parse, $rootScope, $timeout) {
+app.directive('commentDisplay', ['$log', 'User', 'Api', '$parse', '$rootScope', '$timeout', '$translate', 
+	function($log, User, Api, $parse, $rootScope, $timeout, $translate) {
 		return {
 			restrict: 'E',
 			transclude: false,
@@ -16,7 +16,11 @@ app.directive('commentDisplay', ['$log', 'User', 'Api', '$parse', '$rootScope', 
 			link: function ($scope, element, attrs) {
 			},
 			controller: function($scope) {
-
+				$scope.sortOptions = [
+					{ "value" : "byturn", "label" : "<span>" + $translate.instant('global.review.comment.sort.byturn') + "</span>" },
+					{ "value" : "best", "label" : "<span>" + $translate.instant('global.review.comment.sort.best') + "</span>" }
+				]
+				$scope.review.commentSortCriteria = 'byturn'
 			}
 		}
 	}

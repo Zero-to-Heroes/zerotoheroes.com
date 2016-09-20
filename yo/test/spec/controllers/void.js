7d296f793561eb808599211260b7722ab88369a5
sebastientromp <sebastien.tromp@gmail.com>
2016-09-20 22:17:56 +0200

    Fix test dependencies

--- yo/test/spec/controllers/void.js
+++ yo/test/spec/controllers/void.js
@@ -2,19 +2,19 @@
 
 describe('Controller: HomePageCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('app'));
+	// load the controller's module
+	beforeEach(module('app'));
 
-  var HomePageCtrl, scope;
+	var HomePageCtrl, scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    HomePageCtrl = $controller('HomePageCtrl', {
-      $scope: scope
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		HomePageCtrl = $controller('HomePageCtrl', {
+			$scope: scope
+		});
+	}));
 
-  it('should do nothing', function () {
-  });
+	it('should do nothing', function () {
+	});
 });

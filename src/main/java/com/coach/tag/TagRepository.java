37f065afe17f59e7bb4a0786e630057d2f81331a
sebastientromp <sebastien.tromp@gmail.com>
2016-10-31 15:06:26 +0100

    Add missing game modes

--- src/main/java/com/coach/tag/TagRepository.java
+++ src/main/java/com/coach/tag/TagRepository.java
@@ -127,8 +127,11 @@ public class TagRepository {
 		hearthstone.add(new Tag("Arena 11 wins", "skill-level"));
 		hearthstone.add(new Tag("Arena 12 wins", "skill-level"));
 		hearthstone.add(new Tag("Casual", "skill-level"));
+		hearthstone.add(new Tag("Friendly", "skill-level"));
 		hearthstone.add(new Tag("Tavern Brawl", "skill-level"));
 		hearthstone.add(new Tag("Tournament", "skill-level"));
+		hearthstone.add(new Tag("Adventure", "skill-level"));
+		hearthstone.add(new Tag("Innkeeper", "skill-level"));
 		// Decks
 		hearthstone.add(new Tag("Face Hunter"));
 		hearthstone.add(new Tag("Hunter Beast"));

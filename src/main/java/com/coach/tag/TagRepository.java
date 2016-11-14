9ffe58a7a6e804a40c9cc9a8a3512eb06744cf50
sebastientromp <sebastien.tromp@gmail.com>
2016-11-14 16:03:21 +0100

    Change palamurloc tag into Murloc Paladin

--- src/main/java/com/coach/tag/TagRepository.java
+++ src/main/java/com/coach/tag/TagRepository.java
@@ -158,7 +158,7 @@ public class TagRepository {
 		hearthstone.add(new Tag("Aggro Paladin"));
 		hearthstone.add(new Tag("NZoth Paladin"));
 		hearthstone.add(new Tag("Divine Shield Paladin"));
-		hearthstone.add(new Tag("Palamurloc"));
+		hearthstone.add(new Tag("Murloc Paladin"));
 		hearthstone.add(new Tag("Midrange Paladin"));
 		hearthstone.add(new Tag("Dragon Paladin"));
 		hearthstone.add(new Tag("Egg Paladin"));

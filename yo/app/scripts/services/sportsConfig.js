ed309a477ece91e76d7a604ff601f80a464e87d4
sebastientromp <sebastien.tromp@gmail.com>
2017-01-05 08:03:22 +0100

    Fix TB parsing issue

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -63,7 +63,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse', 'localStorage
 						plugins: [
 							{name: 'parseCardsText', version: 29, dev: dev}, 
 							{name: 'parseDecks', version: 42, dev: dev}, 
-							{name: 'manastorm', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 141, dev: dev},
+							{name: 'manastorm', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 142, dev: dev},
 							{name: 'windrunner', player: true, mediaType: 'arena-draft', version: 32, dev: dev}
 						],
 						customCss: 'hearthstone.css?7'

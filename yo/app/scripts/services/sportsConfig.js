6c0683a2ffe2f2f7cfa21558be0a0046d03f6f39
sebastientromp <sebastien.tromp@gmail.com>
2017-03-20 22:19:59 +0100

    Upgrade manastorm to 1.0.152

--- yo/app/scripts/services/sportsConfig.js
+++ yo/app/scripts/services/sportsConfig.js
@@ -63,7 +63,7 @@ services.factory('SportsConfig', ['$log', 'angularLoad', '$parse', 'localStorage
 						plugins: [
 							{name: 'parseCardsText', version: 31, dev: dev}, 
 							{name: 'parseDecks', version: 42, dev: dev}, 
-							{name: 'manastorm', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 151
+							{name: 'manastorm', player: true, format: ['text/xml'], mediaType: 'game-replay', version: 152
 							, dev: dev},
 							{name: 'windrunner', player: true, mediaType: 'arena-draft', version: 32, dev: dev}
 						],

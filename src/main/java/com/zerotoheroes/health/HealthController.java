ffbc320cf232684d9e5e01fe3d705e1cfe40bfdd
sebastientromp <sebastien.tromp@gmail.com>
2018-01-23 18:19:34 +0100

    Add dedicated health controller for AWS ELB

---
+++ src/main/java/com/zerotoheroes/health/HealthController.java
@@ -0,0 +1,19 @@
+package com.zerotoheroes.health;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RepositoryRestController
+@RequestMapping(value = "/health")
+public class HealthController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public @ResponseBody ResponseEntity<String> checkHealth() {
+		return new ResponseEntity<String>("Health is ok", HttpStatus.OK);
+	}
+}
+

8fff7ff8f7513061ace34e8cb07416d2b1174cc7
sebastientromp <sebastien.tromp@gmail.com>
2018-03-01 10:28:43 +0100

    Clean up a few unused files

--- yo/app/scripts/config.js
+++ yo/app/scripts/config.js
@@ -4,6 +4,6 @@
 
 .constant('ENV', {name:'development',apiEndpoint:'localhost:8080',bucket:'com.zerotoheroes.test.input',folder:'videos',videoStorageUrl:'https://s3-us-west-2.amazonaws.com/com.zerotoheroes.test.output/',reviewInit:'https://husxs4v58a.execute-api.us-west-2.amazonaws.com/prod',createReviewBucket:'com.zerotoheroes.test.batch'})
 
-.constant('version', '20180228-17')
+.constant('version', '20180301-10')
 
 ;

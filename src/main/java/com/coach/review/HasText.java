19409fea523b1f7325b63c86859d7ad24946ef9d
sebastientromp <sebastien.tromp@gmail.com>
2020-01-31 10:22:17 +0100

    delete classes that are not core to review upload

--- src/main/java/com/coach/review/HasText.java
+++
@@ -1,9 +0,0 @@
-package com.coach.review;
-
-public interface HasText {
-
-	String getText();
-
-	void setText(String newText);
-
-}
--- src/main/java/com/coach/subscription/SavedSearchSubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coach.subscription;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SavedSearchSubscriptionRepository extends MongoRepository<SavedSearchSubscription, String> {
-
-}
